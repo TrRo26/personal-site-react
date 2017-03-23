ad37a19fd12f9b3df69af1b2a563d94583e8eaaf
Travis Roy <travis-mac@Travis-iMac.hsd1.il.comcast.net>
2017-03-23 15:04:53 -0500

    Fix environment issues - all good now

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # personal-site-react
 Personal website/portfolio built with React
+
+
+NOTES
+
+npm run build (shortcut referenced in package.json)
+npm run start
---
+++ components/app.js
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div id='main-nav'>
+          <h1 id='name-text'> TRAVIS A. ROY </h1>
+          <h2 id='title-text'> [ WEB DEVELOPER ] </h2>
+        </div>
+        <div id='main-content'>
+        
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = App
