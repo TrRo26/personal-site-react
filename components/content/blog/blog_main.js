82504d9adacfd59fcb7c42d9386918e5d0d56a08
Travis Roy <travis-mac@Travis-iMac.hsd1.il.comcast.net>
2017-03-24 17:23:52 -0500

    Format projects

--- components/content/blog/blog_main.js
+++ components/content/blog/blog_main.js
@@ -2,12 +2,25 @@ var React = require('react')
 
 var BlogMain = React.createClass({	
 	render() {
+		var title = {
+			textAlign: 'center',
+			fontSize: 30
+		}
+		var aboutContainer = {
+      margin: '1%',
+      marginLeft: '5%',
+      marginRight: '5%',
+      padding: '10px'
+    }
 		return(
 			<div>
-				<h3>Blog</h3>
-				<p>
-					Eventually...blog posts will go here.
-				</p>
+
+				<h1 style={title}>BLOG</h1>
+
+				<div id='summary' style={aboutContainer}>
+					<p>Eventually...blog posts will go here.</p>
+				</div>
+				
 			</div>
 		)
 	}
--- components/content/projects/projects_main.js
+++ components/content/projects/projects_main.js
@@ -2,28 +2,48 @@ var React = require('react')
 
 var ProjectMain = React.createClass({
 	render() {
+		var title = {
+			textAlign: 'center',
+			fontSize: 30
+		}
+		var aboutContainer = {
+			border: '1px solid black',
+      margin: '1%',
+      marginLeft: '5%',
+      marginRight: '5%',
+      padding: '10px'
+    }
 		return(
 			<div>
-				<h3>PROJECTS</h3>
-				<p>
-				Remind
-				Project descriptionTech: Ruby on Rails, Xcode, Swift, Google Maps API and Heroku
-				Timeframe: 7 days building; 2 days finalizing features, prep and presentation
-				Notable constraints: Zero previous Swift or Xcode experience for the collective team
 
-				Remind is an iOS application that notifies users when they are near a business/location that offers any of the items entered on their personal and customizable list. 
+				<h1 style={title}>PROJECTS</h1>
+			
+				<div id='projectRemind' style={aboutContainer}>
+					<h2>Remind</h2>
+					<h3>Tech:</h3>
+					<p>Ruby on Rails, Xcode, Swift, Google Maps API and Heroku</p>
+					<h3>Timeframe:</h3>
+					<p>7 days building; 2 days finalizing features, prep and presentation</p>
+					<h3>Notable constraints:</h3>
+					<p>Zero previous Swift or Xcode experience for the collective team</p>
+					<h3>Description</h3>
+					<p>Remind is an iOS application that notifies users when they are near a business/location that offers any of the items entered on their personal and customizable list.</p>
+					<p>My main focus throughout the project was in researching and implementing the Core Motion Framework used to constrain push notifications to a set distance moved by the user, preventing extraneous notifications. As a result, I gained a solid foundation in Xcode and Swift and a broader understanding of mobile development as a whole.</p>
+				</div>
+			
+				<div id='projectBattleship' style={aboutContainer}>
+					<h2>Battleship Clone</h2>
+					<h3>Tech:</h3>
+					<p>Ruby, Rails, JQuery, AJAX</p>
+					<h3>Timeframe:</h3>
+					<p>3 days</p>
+					<h3>Notable constraints:</h3>
+					<p>Time</p>
+					<h3>Description</h3>
+					<p>A rudimentary clone of the classic game, Battleship. This remake allows two players to play one another using two separate devices.</p>
+					<p>My main contribution to this project was in setting up both backend and frontend support for user stats as well as aiding in the development of needed asynchronous requests.</p>
+				</div>
 
-				My main focus throughout the project was in researching and implementing the Core Motion Framework used to constrain push notifications to a set distance moved by the user, preventing extraneous notifications. As a result, I gained a solid foundation in Xcode and Swift and a broader understanding of mobile development as a whole.
-
-
-				Battleship Clone
-				Project descriptionTech: Ruby, Rails, JQuery, AJAX
-				Timeframe: 3 days
-
-				A rudimentary clone of the classic game, Battleship. This remake allows two players to play one another using two separate devices.
-
-				My main contribution to this project was in setting up both backend and frontend support for user stats as well as aiding in the development of needed asynchronous requests.
-				</p>
 			</div>
 		)
 	}
